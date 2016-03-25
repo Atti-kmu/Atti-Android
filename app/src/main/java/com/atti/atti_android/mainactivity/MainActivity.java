7101462d60fc04f419e87a972ff5fc8fac33aba0
KimBoWoon <haioer@naver.com>
2016-03-25 10:57:09 +0900

    Android Bootstrap Apply & activity_main Modify

--- app/src/main/java/com/atti/atti_android/mainactivity/MainActivity.java
+++ app/src/main/java/com/atti/atti_android/mainactivity/MainActivity.java
@@ -1,35 +1,14 @@
 package com.atti.atti_android.mainactivity;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.atti.atti_android.R;
 import com.atti.atti_android.playrtc.PlayRTCDisplay;
-import com.sktelecom.playrtc.PlayRTC;
-import com.sktelecom.playrtc.PlayRTCFactory;
-import com.sktelecom.playrtc.config.PlayRTCConfig;
-import com.sktelecom.playrtc.config.PlayRTCSettings;
-import com.sktelecom.playrtc.config.PlayRTCVideoConfig;
-import com.sktelecom.playrtc.exception.RequiredConfigMissingException;
-import com.sktelecom.playrtc.exception.RequiredParameterMissingException;
-import com.sktelecom.playrtc.exception.UnsupportedPlatformVersionException;
-import com.sktelecom.playrtc.observer.PlayRTCObserver;
-import com.sktelecom.playrtc.stream.PlayRTCMedia;
-import com.sktelecom.playrtc.util.android.PlayRTCAudioManager;
-import com.sktelecom.playrtc.util.ui.PlayRTCVideoView;
-
-import org.json.JSONObject;
-
-import java.io.File;
+import com.beardedhen.androidbootstrap.TypefaceProvider;
 
 public class MainActivity extends Activity {
     @Override
@@ -37,16 +16,28 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.start_view).setOnClickListener(listener);
+        TypefaceProvider.registerDefaultIconSets();
+
+        findViewById(R.id.btn_family).setOnClickListener(listener);
+        findViewById(R.id.btn_friend).setOnClickListener(listener);
+        findViewById(R.id.btn_social_worker).setOnClickListener(listener);
     }
 
     Button.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
-                case R.id.start_view:
-                    Intent playRTCView = new Intent(MainActivity.this, PlayRTCDisplay.class);
-                    startActivity(playRTCView);
+                case R.id.btn_family:
+                    Intent familyList = new Intent(MainActivity.this, PlayRTCDisplay.class);
+                    startActivity(familyList);
+                    break;
+                case R.id.btn_friend:
+                    Intent friendList = new Intent(MainActivity.this, PlayRTCDisplay.class);
+                    startActivity(friendList);
+                    break;
+                case R.id.btn_social_worker:
+                    Intent socialWorkerList = new Intent(MainActivity.this, PlayRTCDisplay.class);
+                    startActivity(socialWorkerList);
                     break;
                 default:
                     break;
