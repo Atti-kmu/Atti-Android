40e4b6d1c07d0870030e14f021cdc8ee66e3467b
김보운 <haioer@naver.com>
2016-03-28 18:12:00 +0900

    added DataManager

---
+++ app/src/main/java/com/atti/atti_android/data/FamilyDataManager.java
@@ -0,0 +1,16 @@
+package com.atti.atti_android.data;
+
+import com.atti.atti_android.person.User;
+
+import java.util.ArrayList;
+
+/**
+ * Created by BoWoon on 2016-03-28.
+ */
+public class FamilyDataManager {
+    private ArrayList<User> users;
+
+    public FamilyDataManager() {
+        users = new ArrayList<User>();
+    }
+}
---
+++ app/src/main/java/com/atti/atti_android/data/SocialWorkerDataManager.java
@@ -0,0 +1,16 @@
+package com.atti.atti_android.data;
+
+import com.atti.atti_android.person.User;
+
+import java.util.ArrayList;
+
+/**
+ * Created by BoWoon on 2016-03-28.
+ */
+public class SocialWorkerDataManager {
+    private ArrayList<User> users;
+
+    public SocialWorkerDataManager() {
+        users = new ArrayList<User>();
+    }
+}
