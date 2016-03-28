e3533346a8082859640448f5dd524b1e2c8e2dc4
gminmin <kmmin11@naver.com>
2016-03-28 19:31:21 +0900

    User Data Update

---
+++ app/src/main/java/com/atti/atti_android/person/ElderlyPerson.java
@@ -0,0 +1,8 @@
+package com.atti.atti_android.person;
+
+/**
+ * Created by LG on 2016-03-28.
+ */
+public class ElderlyPerson extends CommonPerson {
+    public ElderlyPerson(String name, String phoneNumber, String nickName) { super(name, phoneNumber, nickName); }
+}
---
+++ app/src/main/java/com/atti/atti_android/person/SocialWorker.java
@@ -0,0 +1,8 @@
+package com.atti.atti_android.person;
+
+/**
+ * Created by LG on 2016-03-28.
+ */
+public class SocialWorker extends CommonPerson {
+    public SocialWorker(String name, String phoneNumber) { super(name, phoneNumber); }
+}
